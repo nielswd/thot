2c98f7e5eb1f51cce8d0be0d2165b34b5834ca0c
nielswd <tzada.rock@gmail.com>
2014-02-06 19:35:09 +0100

    Update printdir.php

--- printdir.php
+++ printdir.php
@@ -1,3 +1,13 @@
 <?php
-print_r(scandir('.'));
+$fichier = file('82cdb247144509ccf3946e4aaad15614.txt');
+
+$total = count($fichier); // Nombre total des lignes du fichier
+
+for($i = 0; $i < $total; $i++) 
+{ // Départ de la boucle
+
+echo $fichier[$i]; // On affiche ligne par ligne le contenu du fichier
+
+} // Fin de la boucle
+
 ?>
