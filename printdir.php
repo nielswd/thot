3b58447342b4a0edfca0ea97b89f0474fc308fc4
nielswd <tzada.rock@gmail.com>
2014-02-06 19:26:29 +0100

    Update printdir.php

--- printdir.php
+++ printdir.php
@@ -1,3 +1,13 @@
 <?php
-print_r(readfile(e3c23645298cfcb355466bbf58012fea.txt));
+$fichier = file("e3c23645298cfcb355466bbf58012fea.txt"); // Nom du fichier à afficher
+ 
+$total = count($fichier); // Nombre total des lignes du fichier
+
+for($i = 0; $i < $total; $i++) 
+{ // Départ de la boucle
+
+echo $fichier[$i]; // On affiche ligne par ligne le contenu du fichier
+
+} // Fin de la boucle
+
 ?>
